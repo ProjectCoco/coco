5d6e7dfa550701be0658a37f1d49bcf24d24c072
devfrank9 <devfrank9@gmail.com>
2022-05-06 02:34:54 +0900

    Feat: "Router 설정"
    우선 SPA 구성 위해 프론트 단 라우터 구성.
    추가적으로 Navi헤더 및 BaseScreen으로 공통적으로 보여지는 콘텐츠 단 스타일링

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,12 +1,29 @@
 import { Route, Routes } from "react-router-dom";
+import styled from "styled-components";
+import StudyBoard from "./pages/StudyBoard";
 import Main from "./pages/Main";
+import Login from "./pages/Login";
+import NaviHeader from "./components/NaviHeader";
 
 const Router = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-    </Routes>
+    <>
+      <NaviHeader />
+      <BaseScreen>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/study-board" element={<StudyBoard />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BaseScreen>
+    </>
   );
 };
+
+const BaseScreen = styled.div`
+  width: 100%;
+  max-width: 48rem;
+  margin: 0 auto;
+`;
 
 export default Router;
