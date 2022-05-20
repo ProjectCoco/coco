7a080849d6a82e4ab21e0a6800a8e36e45489c6d
seonnn <ssni0105@gmail.com>
2022-05-20 21:13:54 +0900

    feat: 추가 글쓰기 페이지

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,31 +1,25 @@
-import { Route, Routes } from 'react-router-dom';
-import styled from 'styled-components';
-import StudyBoard from './pages/StudyBoard';
-import Main from './pages/Main';
-import Login from './pages/Login';
-import NaviHeader from './components/NaviHeader';
-import StudyBoardDetail from './pages/StudyBoardDetail';
+import { Route, Routes } from "react-router-dom";
+import styled from "styled-components";
+import StudyBoard from "./pages/StudyBoard";
+import Main from "./pages/Main";
+import Login from "./pages/Login";
+import NaviHeader from "./components/NaviHeader";
+import StudyBoardDetail from "./pages/StudyBoardDetail";
+import StudyBoardWrite from "./pages/StudyBoardWrite";
 
 const Router = () => {
   return (
     <>
       <NaviHeader />
-      {/* <BaseScreen> */}
       <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='/study-board' element={<StudyBoard />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/study-board/:id' element={<StudyBoardDetail />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/study-board" element={<StudyBoard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/study-board/:id" element={<StudyBoardDetail />} />
+        <Route path="/study-board-write" element={<StudyBoardWrite />} />
       </Routes>
-      {/* </BaseScreen> */}
     </>
   );
 };
-
-const BaseScreen = styled.div`
-  width: 100%;
-  max-width: 48rem;
-  margin: 0 auto;
-`;
 
 export default Router;
