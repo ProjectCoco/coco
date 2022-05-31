f91cf7ba79a0329f541e3b5fc6ac252f612650a6
JEONGHWANMIN <mimi0j0j@naver.com>
2022-06-01 00:12:04 +0900

    Fix: "리액트 버전 다운 그레이드"
    * 토스트 유아이 버전 일치 하기 위한 다운그레이드

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
 import { theme } from './styles/theme';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
