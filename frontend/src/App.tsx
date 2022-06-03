ffde692eb360996df866a3bb8413d372de3f1565
devfrank9 <devfrank9@gmail.com>
2022-06-03 18:58:08 +0900

    Feat: "Recoil 전역 설정"
    Recoil사용 가능하도록 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,17 @@ import { GlobalStyle } from './styles/GlobalStyle';
 import Router from './Router';
 import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
+import { RecoilRoot } from 'recoil';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <GlobalStyle />
-        <Router />
-      </BrowserRouter>
+      <RecoilRoot>
+        <BrowserRouter>
+          <GlobalStyle />
+          <Router />
+        </BrowserRouter>
+      </RecoilRoot>
     </>
   );
 }
