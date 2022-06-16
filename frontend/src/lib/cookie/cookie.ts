f5d29046c26a8fd33ea2b2d56fddaf4fc7576f0b
JeongHwanMin <65848374+JEONGHWANMIN@users.noreply.github.com>
2022-06-16 18:37:35 +0900

    Feat : "추가 쿠키 모듈"

--- frontend/src/lib/cookie/cookie.ts
+++ frontend/src/lib/cookie/cookie.ts
@@ -2,10 +2,18 @@ import { Cookies } from 'react-cookie';
 
 const cookies = new Cookies();
 
-export const setCookie = (name: string, value: string, option?: any) => {
+export const setCookie = (
+  name: string,
+  value: string,
+  option?: { path: string }
+) => {
   return cookies.set(name, value, { ...option });
 };
 
 export const getCookie = (name: string) => {
   return cookies.get(name);
+};
+
+export const removeCookie = (name: string) => {
+  return cookies.remove(name);
 };
