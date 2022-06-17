153682292e449b981bd5d468581afd9aa4096b48
devfrank9 <devfrank9@gmail.com>
2022-06-17 12:00:31 +0900

    Feat: "DB의 데이터 가져오기"
    상태로 보드목록 가져오기

--- frontend/src/pages/StudyBoard/StudyBoard.tsx
+++ frontend/src/pages/StudyBoard/StudyBoard.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './style';
 import ContentLayout from '../../components/ContentLayout';
-
-//dummies
-import { DuBoardList } from '../../dummys/dummy';
 import BoardContentBox from '../../components/BoardContentBox';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { StudyBoardList, __isLoading } from '../../lib/atom';
 
 const StudyBoard = () => {
+  const [boardList, setBoardList] = useState([]);
+  const getBoard = useRecoilValue(StudyBoardList);
+  const [isLoading, setIsLoading] = useRecoilState(__isLoading);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setBoardList(getBoard);
+    if (boardList !== null) setIsLoading(false);
+  }, [boardList, getBoard, setIsLoading]);
+
   return (
     <ContentLayout>
       <S.Body>
         <S.Content>
           <S.BoardListContainer>
-            {DuBoardList.slice(0, 10).map((data, index) => (
+            {boardList.map((data, index) => (
               <BoardContentBox key={index} data={data} />
             ))}
           </S.BoardListContainer>
