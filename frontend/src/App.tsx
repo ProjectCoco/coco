c062a5b9e299888a2003037c5b7b52b66b96d079
devfrank9 <devfrank9@gmail.com>
2022-06-17 11:59:15 +0900

    Feat: "로딩 suspence 추가"
    우선 data fetch시 보호되는 로직이 있어야 함으로 보임 따라서 우선 suspence로 로딩 스크린 삽입.
    
    TODO: recoilvalue로 loading의 boolean을 제어하는데 이게 정확히 작동하는가?

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,23 @@
 import { GlobalStyle } from './styles/GlobalStyle';
 import Router from './Router';
 import { BrowserRouter } from 'react-router-dom';
-import React from 'react';
-import { RecoilRoot, useRecoilValue } from 'recoil';
+import React, { Suspense } from 'react';
+import { useRecoilValue } from 'recoil';
 import { CookiesProvider } from 'react-cookie';
 import { __isLoading } from './lib/atom';
 import Loading from './components/Loading';
 
-const LoadingHooker = () => {
-  const isLoading = useRecoilValue(__isLoading);
-  return isLoading ? <Loading /> : null;
-};
-
 const App = () => {
+  const isLoading = useRecoilValue(__isLoading);
   return (
-    <>
-      <CookiesProvider>
-        <RecoilRoot>
-          <BrowserRouter>
-            <LoadingHooker />
-            <GlobalStyle />
-            <Router />
-          </BrowserRouter>
-        </RecoilRoot>
-      </CookiesProvider>
-    </>
+    <CookiesProvider>
+      <Suspense fallback={isLoading ? <Loading /> : null}>
+        <BrowserRouter>
+          <GlobalStyle />
+          <Router />
+        </BrowserRouter>
+      </Suspense>
+    </CookiesProvider>
   );
 };
 
