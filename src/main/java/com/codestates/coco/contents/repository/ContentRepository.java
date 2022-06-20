337172aa1735ef70c222930e650968fd7c9b4168
minoolian <mw.lee@kakao.com>
2022-06-20 23:54:37 +0900

    Fix: "정리 되지 않은 부분 수정"

--- src/main/java/com/codestates/coco/contents/repository/ContentRepository.java
+++ src/main/java/com/codestates/coco/contents/repository/ContentRepository.java
@@ -1,9 +1,9 @@
 package com.codestates.coco.contents.repository;
+
 import com.codestates.coco.contents.domain.Content;
-import com.codestates.coco.contents.domain.ContentGetDTO;
+import com.codestates.coco.contents.domain.ContentDTO;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
-
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -13,5 +13,8 @@ import java.util.List;
 @Repository
 public interface ContentRepository extends MongoRepository<Content, String> {
     @Query(sort = "{_id:-1}")
-    List<ContentGetDTO> findBy(Pageable pageable);
+    List<ContentDTO> findBy(Pageable pageable);
+
+//    List<ContentDTO> findAllBy();
+
 }
