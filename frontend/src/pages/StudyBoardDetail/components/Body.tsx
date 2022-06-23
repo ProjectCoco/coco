c7635c557371f93b21178d392b4491f4a953b4a5
devfrank9 <devfrank9@gmail.com>
2022-06-23 18:53:08 +0900

    Feat: "게시판 상세 API연결"
    id 파라미터 받아서 query api 추가

--- frontend/src/pages/StudyBoardDetail/components/Body.tsx
+++ frontend/src/pages/StudyBoardDetail/components/Body.tsx
@@ -1,36 +1,42 @@
 import React, { useState } from 'react';
 import * as S from '../style';
-import { IDuBoardList } from '../../../dummys/dummy';
+import { IDuBoardList, IDuComment } from '../../../lib/types';
 import commentImg from '../../../images/userProfile.jpg';
 import { AiOutlineComment } from 'react-icons/ai';
 import { postCommentApi } from '../../../apis/apiClient';
 import { useRecoilValue } from 'recoil';
 import { UserState } from '../../../lib/atom';
+import { useQuery } from 'react-query';
+
 type DataProps = {
-  data: IDuBoardList;
+  board: IDuBoardList;
 };
 
-function Body({ data }: DataProps) {
+function Body({ board }: DataProps) {
+  const fetchComment = async () => {
+    const res = await fetch(`http://localhost:8080/comment/${board._id}`);
+    return res.json();
+  };
+  const { data } = useQuery('comment', fetchComment);
   const [comment, setComment] = useState<string>('');
   const user = useRecoilValue(UserState);
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const commentForm = {
+      _id: board._id,
       author: user.email,
       comment: comment,
+      createdDate: new Date(Date.now()).toISOString(),
     };
     const response = await postCommentApi(commentForm);
+    if (response) setComment('');
     console.log(response);
-  }
-
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setComment(e.target.value);
   }
 
   return (
     <S.Body>
-      <S.Content>{data?.content}</S.Content>
+      <S.Content>{board.content}</S.Content>
       <S.CommentBox id="CommentBox">
         <S.CommentLength>
           <AiOutlineComment />
@@ -40,33 +46,36 @@ function Body({ data }: DataProps) {
           <S.CommentInput
             type={'text'}
             placeholder={
-              user.email.length >= 1
+              user !== undefined
                 ? '댓글을 입력해 주세요.'
                 : '댓글을 입력하려면 로그인을 해주세요.'
             }
-            onChange={handleChange}
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
             disabled={user.email.length > 1 ? false : true}
           />
           <S.Button>입력</S.Button>
         </S.CommentForm>
-        {data?.comment.map((obj) =>
-          obj.content.map((comment, idx) => (
-            <S.ShowComment key={idx}>
+        {data === undefined ? (
+          <></>
+        ) : (
+          data.map((comment: IDuComment) => (
+            <S.ShowComment key={comment._id}>
               <S.CommentProfile>
                 <div>
                   <S.CommentImg src={commentImg} />
                 </div>
                 <div>
-                  <h1>ghks4{comment.id}</h1>
-                  <h3>2022-05-22</h3>
+                  <h1>{comment.author}</h1>
+                  <h3>{comment.createdDate}</h3>
                 </div>
               </S.CommentProfile>
-              <p>{comment.content}</p>
+              <p>{comment.comment}</p>
             </S.ShowComment>
           ))
         )}
       </S.CommentBox>
-      <S.Blank></S.Blank>
+      <S.Blank />
     </S.Body>
   );
 }
