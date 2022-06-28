ba96a862e12e8bc3faa3c991e0f58950c895d37b
devfrank9 <devfrank9@gmail.com>
2022-06-29 00:03:13 +0900

    Feat: "코멘트 권한 설정, 버튼 등"

--- frontend/src/pages/StudyBoardDetail/components/Body.tsx
+++ frontend/src/pages/StudyBoardDetail/components/Body.tsx
@@ -2,22 +2,32 @@ import React, { useState } from 'react';
 import * as S from '../style';
 import { IDuBoardList, IDuComment } from '../../../lib/types';
 import commentImg from '../../../images/userProfile.jpg';
-import { AiOutlineComment } from 'react-icons/ai';
+import { AiOutlineComment, AiFillEdit, AiOutlineDelete } from 'react-icons/ai';
 import { postCommentApi } from '../../../apis/apiClient';
 import { useRecoilValue } from 'recoil';
 import { UserState } from '../../../lib/atom';
 import { Viewer } from '@toast-ui/react-editor';
+import axios from 'axios';
 
 interface DataProps {
   board: IDuBoardList;
   comment: IDuComment[];
 }
 
-function Body({ board, comment }: DataProps) {
+const removeComment = async (id: string) => {
+  return await axios.delete(`http://localhost:8080/api/comment/${id}`);
+};
+const putComment = async (id: string, data: string) => {
+  return await axios.put(`http://localhost:8080/api/comment/${id}`, data);
+};
+
+const Body = ({ board, comment }: DataProps) => {
   const [string, setString] = useState<string>('');
+  const [editString, setEditString] = useState('');
+  const [edit, setEdit] = useState(false);
   const user = useRecoilValue(UserState);
 
-  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const commentForm = {
       contentId: board._id,
@@ -25,9 +35,7 @@ function Body({ board, comment }: DataProps) {
       comment: string,
       createdDate: new Date(Date.now()).toISOString(),
     };
-    const response = await postCommentApi(commentForm);
-    if (response) setString('');
-    console.log(response);
+    async () => await postCommentApi(commentForm).then(() => setString(''));
   }
 
   return (
@@ -37,13 +45,14 @@ function Body({ board, comment }: DataProps) {
       </S.Content>
       <S.CommentBox id="CommentBox">
         <S.CommentLength>
-          <AiOutlineComment onClick={() => console.log(comment)} />
+          <AiOutlineComment />
+          <p>{comment.length ?? 0}</p>
         </S.CommentLength>
         <S.CommentForm onSubmit={handleSubmit}>
           <S.CommentInput
             type={'text'}
             placeholder={
-              user !== undefined
+              user.email.length > 1
                 ? '댓글을 입력해 주세요.'
                 : '댓글을 입력하려면 로그인을 해주세요.'
             }
@@ -53,28 +62,48 @@ function Body({ board, comment }: DataProps) {
           />
           <S.Button>입력</S.Button>
         </S.CommentForm>
-        {comment === undefined ? (
-          <></>
-        ) : (
-          comment.map((comment) => (
-            <S.ShowComment key={comment._id}>
-              <S.CommentProfile>
-                <div>
-                  <S.CommentImg src={commentImg} />
-                </div>
-                <div>
-                  <h1>{comment.author}</h1>
-                  <h3>{comment.createdDate}</h3>
-                </div>
-              </S.CommentProfile>
+        {comment.map((comment) => (
+          <S.ShowComment key={comment._id}>
+            <S.CommentProfile>
+              <div>
+                <S.CommentImg src={commentImg} />
+              </div>
+              <div>
+                <h1>{comment.author}</h1>
+                <h3>{comment.createdDate}</h3>
+              </div>
+              {user.email === comment.author ? (
+                <S.IconAlign>
+                  <AiFillEdit onClick={() => setEdit((prev) => !prev)} />
+                  <AiOutlineDelete onClick={() => removeComment(comment._id)} />
+                </S.IconAlign>
+              ) : (
+                <></>
+              )}
+            </S.CommentProfile>
+            {edit ? (
+              <S.EditComment>
+                <textarea
+                  value={comment.comment}
+                  onChange={(e) => setEditString(e.target.value)}
+                />
+                <S.Button
+                  onClick={async () =>
+                    await putComment(comment._id, editString)
+                  }
+                >
+                  수정완료
+                </S.Button>
+              </S.EditComment>
+            ) : (
               <p>{comment.comment}</p>
-            </S.ShowComment>
-          ))
-        )}
+            )}
+          </S.ShowComment>
+        ))}
       </S.CommentBox>
       <S.Blank />
     </S.Body>
   );
-}
+};
 
 export default Body;
--- frontend/src/pages/StudyBoardDetail/components/Header.tsx
+++ frontend/src/pages/StudyBoardDetail/components/Header.tsx
@@ -1,17 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from '../style';
 import { IDuBoardList } from '../../../lib/types/index';
 import { MdOutlineFavoriteBorder, MdFavorite } from 'react-icons/md';
 // 더미데이터
 import profileImg2 from '../../../images/download.jpg';
-import Loading from '../../../components/Loading';
+import axios from 'axios';
+import { useRecoilValue } from 'recoil';
+import { UserState } from '../../../lib/atom';
 
 interface DataProps {
   board: IDuBoardList;
 }
 
+// 헤더 고쳐진거 보고 넣자
+const favorPut = async (id: string, data: number) => {
+  return await axios.put(
+    `http://localhost:8080/api/content/${id}`,
+    JSON.stringify(data)
+  );
+};
+
 const Header = ({ board }: DataProps) => {
   const parseDate = new Date(board?.createdDate);
+  const [like, setLike] = useState(false);
+  const user = useRecoilValue(UserState);
+
+  useEffect(() => {
+    if (user.email !== null) {
+      if (like) favorPut(board._id, board.favor + 1);
+      else favorPut(board._id, board.favor - 1);
+    }
+  }, [like]);
 
   return (
     <S.Header>
@@ -29,8 +48,12 @@ const Header = ({ board }: DataProps) => {
           </div>
         </S.UserBox>
         <S.FavoritBox>
-          {board?.favor ? <MdFavorite /> : <MdOutlineFavoriteBorder />}
-          <h4>{board?.favor === null ? '0' : board?.favor}</h4>
+          {like ? (
+            <MdFavorite onClick={() => setLike(false)} />
+          ) : (
+            <MdOutlineFavoriteBorder onClick={() => setLike(true)} />
+          )}
+          <h4>{board?.favor ?? '0'}</h4>
         </S.FavoritBox>
       </S.UserLogo>
       <S.Subject>{board?.title}</S.Subject>
