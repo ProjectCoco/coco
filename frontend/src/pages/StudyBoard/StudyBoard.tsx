dae710b6262e889c67d1b117cdf11f561d073283
devfrank9 <devfrank9@gmail.com>
2022-06-29 00:23:17 +0900

    Refactor: "코드 정리"
    TODO: 코멘트 길이 어떻게 받아오지

--- frontend/src/pages/StudyBoard/StudyBoard.tsx
+++ frontend/src/pages/StudyBoard/StudyBoard.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import * as S from './style';
 import ContentLayout from '../../components/ContentLayout';
 import BoardContentBox from '../../components/BoardContentBox';
-import { useInfiniteQuery } from 'react-query';
+import { useInfiniteQuery, useQuery } from 'react-query';
 import { IDuBoardList } from '../../lib/types';
 import Loading from '../../components/Loading';
 import NotFound from '../NotFound/NotFound';
@@ -14,35 +14,23 @@ const fetchData = async (page: number) => {
     .get(`http://localhost:8080/api/content?page=${page}`)
     .then((res) => res.data);
 };
+const fetchComment = async (id: string) => {
+  return await axios
+    .get(`http://localhost:8080/api/comment/${id}`)
+    .then((res) => res.data);
+};
 
 const StudyBoard = () => {
-  const { ref, inView } = useInView({
-    threshold: 0.3,
-  });
-
-  const {
-    isLoading,
-    isError,
-    data,
-    fetchNextPage,
-    isFetching,
-    isFetchingNextPage,
-    hasNextPage,
-  } = useInfiniteQuery(
-    ['page'],
-    async ({ pageParam = 0 }) => await fetchData(pageParam),
-    {
-      getNextPageParam: (lastPage, pages) => {
-        console.log(lastPage);
-        return pages.length + 1 ?? undefined;
-      },
-    }
-  );
+  const { ref, inView } = useInView({ threshold: 0.3 });
+  const { isLoading, isError, data, fetchNextPage, hasNextPage } =
+    useInfiniteQuery(
+      ['page'],
+      async ({ pageParam = 0 }) => await fetchData(pageParam),
+      { getNextPageParam: (lastPage, pages) => pages.length + 1 }
+    );
 
   useEffect(() => {
-    if (inView && hasNextPage) {
-      fetchNextPage();
-    }
+    if (inView && hasNextPage) fetchNextPage();
   }, [inView]);
 
   if (isLoading) return <Loading />;
@@ -56,19 +44,18 @@ const StudyBoard = () => {
               {data?.pages.map((group, index) => (
                 <React.Fragment key={index}>
                   {group.map((data: IDuBoardList) => (
-                    <BoardContentBox key={data._id} board={data} />
+                    <BoardContentBox
+                      key={data._id}
+                      board={data}
+                      comment={() => fetchComment(data._id)}
+                    />
                   ))}
                 </React.Fragment>
               ))}
             </S.BoardListContainer>
           </S.Content>
           <S.Blank />
-          <button
-            ref={ref}
-            onClick={() => fetchNextPage()}
-            disabled={!hasNextPage || isFetchingNextPage}
-          />
-          <div>{isFetching && !isFetchingNextPage ? 'Fetching...' : null}</div>
+          <button ref={ref} onClick={() => fetchNextPage()} />
         </S.Body>
       </ContentLayout>
     </>
