5aaf5809a45377ec26cc35a7e221cc06b97f71c2
devfrank9 <devfrank9@gmail.com>
2022-07-06 23:28:50 +0900

    Fix: "스터디 보드 페이지 조회 에러 수정"
    0페이지에서 2페이지로 넘어가는 에러 수정

--- frontend/src/pages/StudyBoard/StudyBoard.tsx
+++ frontend/src/pages/StudyBoard/StudyBoard.tsx
@@ -1,36 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './style';
 import ContentLayout from '../../components/ContentLayout';
 import BoardContentBox from '../../components/BoardContentBox';
-import { useInfiniteQuery, useQuery } from 'react-query';
+import { useInfiniteQuery } from 'react-query';
 import { IDuBoardList } from '../../lib/types';
 import Loading from '../../components/Loading';
 import NotFound from '../NotFound/NotFound';
 import { useInView } from 'react-intersection-observer';
-import axios from 'axios';
-
-const fetchData = async (page: number) => {
-  return await axios
-    .get(`http://localhost:8080/api/content?page=${page}`)
-    .then((res) => res.data);
-};
-const fetchComment = async (id: string) => {
-  return await axios
-    .get(`http://localhost:8080/api/comment/${id}`)
-    .then((res) => res.data);
-};
+import { getBoardPage, getCommentAll } from '../../apis/apiClient';
 
 const StudyBoard = () => {
+  const [num, SetNum] = useState(1);
   const { ref, inView } = useInView({ threshold: 0.3 });
   const { isLoading, isError, data, fetchNextPage, hasNextPage } =
     useInfiniteQuery(
       ['page'],
-      async ({ pageParam = 0 }) => await fetchData(pageParam),
-      { getNextPageParam: (lastPage, pages) => pages.length + 1 }
+      async ({ pageParam = 0 }) => await getBoardPage(pageParam),
+      { getNextPageParam: () => num }
     );
 
   useEffect(() => {
-    if (inView && hasNextPage) fetchNextPage();
+    if (inView && hasNextPage) {
+      fetchNextPage();
+      SetNum(num + 1);
+    }
   }, [inView]);
 
   if (isLoading) return <Loading />;
@@ -43,13 +36,16 @@ const StudyBoard = () => {
             <S.BoardListContainer>
               {data?.pages.map((group, index) => (
                 <React.Fragment key={index}>
-                  {group.map((data: IDuBoardList) => (
-                    <BoardContentBox
-                      key={data._id}
-                      board={data}
-                      comment={() => fetchComment(data._id)}
-                    />
-                  ))}
+                  {group.map((data: IDuBoardList) => {
+                    const get = async () => await getCommentAll(data._id);
+                    return (
+                      <BoardContentBox
+                        key={data._id}
+                        board={data}
+                        comment={get.length}
+                      />
+                    );
+                  })}
                 </React.Fragment>
               ))}
             </S.BoardListContainer>
