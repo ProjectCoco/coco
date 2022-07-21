78db733b40b47945c68e07494d781cb68d48016d
devfrank9 <devfrank9@gmail.com>
2022-07-21 18:05:32 +0900

    Feat: "코멘트 기능 안정화"

--- frontend/src/components/Comment/Comment.tsx
+++ frontend/src/components/Comment/Comment.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { IDuComment, UserStateType } from '../../lib/types';
 import commentImg from '../../images/userProfile.jpg';
 import { AiFillEdit, AiOutlineDelete } from 'react-icons/ai';
@@ -8,11 +8,18 @@ import * as S from './style';
 interface Prop {
   comment: IDuComment;
   user: UserStateType;
+  refetch: () => void;
 }
 
-export default function Comment({ comment, user }: Prop) {
+export default function Comment({ comment, user, refetch }: Prop) {
+  const parseDate = new Date(comment.createdDate);
   const [editString, setEditString] = useState('');
   const [edit, setEdit] = useState(false);
+
+  const handle = useCallback(() => {
+    setEdit((prev) => !prev);
+    setEditString(comment.comment);
+  }, [edit]);
 
   return (
     <>
@@ -22,17 +29,17 @@ export default function Comment({ comment, user }: Prop) {
         </div>
         <div>
           <h1>{comment.username}</h1>
-          <h3>{comment.createdDate}</h3>
+          <h3>
+            {parseDate.toLocaleDateString()}
+            {parseDate.toLocaleTimeString()}
+          </h3>
         </div>
         {user.username === comment.username ? (
           <S.IconAlign>
-            <AiFillEdit
-              style={{ cursor: 'pointer' }}
-              onClick={() => setEdit((prev) => !prev)}
-            />
+            <AiFillEdit style={{ cursor: 'pointer' }} onClick={handle} />
             <AiOutlineDelete
               style={{ cursor: 'pointer' }}
-              onClick={() => removeComment(comment._id)}
+              onClick={() => removeComment(comment._id).then(() => refetch())}
             />
           </S.IconAlign>
         ) : (
@@ -42,7 +49,7 @@ export default function Comment({ comment, user }: Prop) {
       {edit ? (
         <S.EditComment>
           <textarea
-            value={comment.comment || editString}
+            value={editString}
             onChange={(e) => setEditString(e.target.value)}
           />
           <S.Button
@@ -51,6 +58,8 @@ export default function Comment({ comment, user }: Prop) {
                 ...comment,
                 comment: editString,
               })
+                .then(() => setEdit(false))
+                .then(() => refetch())
             }
           >
             수정완료
