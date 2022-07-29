fe0a2cf75659de0ee36bb3e31e52f0e6b4fe6f99
devfrank9 <devfrank9@gmail.com>
2022-07-29 18:40:24 +0900

    Feat: "favicon경로잡기"

--- frontend/config/webpack.common.js
+++ frontend/config/webpack.common.js
@@ -20,6 +20,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: `${path.resolve(__dirname, '../public')}/index.html`,
+      favicon: `${path.resolve(__dirname, '../public')}/favicon.png`,
     }),
     new webpack.ProvidePlugin({
       React: 'react',
