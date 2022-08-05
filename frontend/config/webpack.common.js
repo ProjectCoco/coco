1cf334f8029ae90dae92ff0690df878b3a0cbe2a
JEONGHWANMIN <mimi0j0j@naver.com>
2022-08-05 16:18:19 +0900

    Feat : "추가 webpack dotenv"

--- frontend/config/webpack.common.js
+++ frontend/config/webpack.common.js
@@ -1,6 +1,9 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 module.exports = {
   entry: `${path.resolve(__dirname, '../src')}/index.tsx`,
@@ -20,10 +23,12 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: `${path.resolve(__dirname, '../public')}/index.html`,
-      favicon: `${path.resolve(__dirname, '../public')}/favicon.png`,
     }),
     new webpack.ProvidePlugin({
       React: 'react',
+    }),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env),
     }),
   ],
   resolve: {
