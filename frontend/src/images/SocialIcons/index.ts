e9a9b0fbeabe36193769eefb52cdc172400de371
JEONGHWANMIN <mimi0j0j@naver.com>
2022-08-19 15:47:35 +0900

    Feat : "이미지 내보내기 파일 추가"

---
+++ frontend/src/images/SocialIcons/index.ts
@@ -0,0 +1,13 @@
+import kakaoLogo from './kakaoIcon1.png';
+import naverLogo from './naverL.png';
+import googleLogo from './icon_google.png';
+import facebookLogo from './facebook.png';
+import githubLogo from './githubLogo.png';
+
+export const SocialIcons = {
+  kakaoLogo,
+  naverLogo,
+  googleLogo,
+  facebookLogo,
+  githubLogo,
+};
